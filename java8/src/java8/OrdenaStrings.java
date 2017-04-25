446d431dac3c4f8180ecf37e88061a7e3b91ec33
maycon <maycon@maycon-pc>
2017-04-24 21:52:26 -0300

    Criação de projeto inicial

---
+++ java8/src/java8/OrdenaStrings.java
@@ -0,0 +1,31 @@
+package java8;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class OrdenaStrings {
+
+	public static void main(String[] args) {
+		
+		List<String> palavras = new ArrayList<>();
+		palavras.add("alura online");
+		palavras.add("casa do código");
+		palavras.add("caelum");
+		
+		Comparator<String> comparador = new ComparadorDeStringPorTamanho();
+		palavras.sort(comparador);
+		
+	}
+	
+}
+
+class ComparadorDeStringPorTamanho implements Comparator<String> {
+    public int compare(String s1, String s2) {
+        if(s1.length() < s2.length()) 
+            return -1;
+        if(s1.length() > s2.length()) 
+            return 1;
+        return 0;
+    }
+}
